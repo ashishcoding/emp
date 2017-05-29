62e26e78ae367c37daa3b7f7f8b4edf2348cea26
ashishcoding <mishraashish185@rediffmail.com>
2017-05-29 16:18:46 +0530

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Mysql.Data.Mysqlclient;
+
+
+namespace employee
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string constring = " Data Source=Local host;port=3306;username=root;password=root";
+           
+            
+            
+        }
+    }
+}
